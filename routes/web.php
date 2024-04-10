70d4688b9ad7dea90106b4b8b6db13b9f10a6913
SiTolap <luckysimarmata5@gmail.com>
2024-04-10 10:09:27 +0700

    ganti link berhasil

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::middleware(['auth'])->group(function ()
     Route::get('/dokumen/mahasiswa', [MahasiswaController::class, 'dokumen']);
     Route::post('/pendaftaran/mahasiswa', [MahasiswaController::class, 'store']);
     Route::get('/file/create', [FileController::class, 'create'])->name('files.create');
-    Route::view('/berhasil', 'user.kirim')->name('berhasil');
-    Route::view('/berhasil', 'user.dokumen_kirim')->name('dokumen_kirim');
+    Route::view('/berhasil-input-data', 'user.kirim')->name('berhasil');
+    Route::view('/berhasil-kirim-dokumen', 'user.dokumen_kirim')->name('dokumen_kirim');
     Route::post('/file', [FileController::class, 'store'])->name('files.store');
 });
 
