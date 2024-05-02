0f01edc6294ce58661f71b31548759bca42216ae
SiTolap <luckysimarmata5@gmail.com>
2024-05-02 10:14:11 +0700

    tambah fitur admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,24 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::id())
+        {
+            $usertype=Auth()->user()->usertype;
+
+            if( $usertype == 'user')
+            {
+                return view('beranda');
+            }
+
+            else if( $usertype == 'admin')
+            {
+                return view('admin.dashboard');
+            }
+
+            else
+            {
+                return redirect()->back();
+            }
+        }
     }
 }
